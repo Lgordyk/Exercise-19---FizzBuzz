f6e32461d9f976cecf5da147655aa8848b9a85d1
Lgordyk <99572889+Lgordyk@users.noreply.github.com>
2022-03-13 13:54:48 -0400

    Add files via upload

---
+++ main (19).py
@@ -0,0 +1,13 @@
+#Write your code below this row 👇
+
+total = 0
+
+for numbers in range(1,101):
+    if numbers % 3 == 0 and numbers % 5 == 0:
+        print("FizzBuzz")
+    elif numbers % 3 == 0 and numbers % 5 != 0:
+        print("Fizz")
+    elif numbers % 3 != 0 and numbers % 5 == 0:
+        print("Buzz")
+    elif numbers % 3 != 0 and numbers % 5 != 0:
+        print(numbers)
